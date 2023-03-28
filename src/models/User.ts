01e5be7be6916a1e83d5143abbcdf5d23d2e4d58
Hayah Hazem <hayahhazem1999@gmail.com>
2023-03-28 19:12:48 +0200

    Adding user schema

---
+++ src/models/User.ts
@@ -0,0 +1,20 @@
+import { Schema, model } from "mongoose";
+
+interface User {
+    firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    avatar: string
+}
+
+const userSchema = new Schema<User>({
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    avatar: { type: String, required: true }
+},
+    { timestamps: true });
+
+export default model<User>('User', userSchema);
