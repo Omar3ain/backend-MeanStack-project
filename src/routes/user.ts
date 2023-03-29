7d4136f933efcd6fa09299bacd970ab4575c3bd3
Hayah Hazem <hayahhazem1999@gmail.com>
2023-03-29 20:51:33 +0200

    modify register function incase of error to delete the image

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,12 +3,13 @@ import RouteInterface from '@/utils/interfaces/router.interface';
 import userController from '@/controllers/user'
 import httpException from '@/utils/exceptions/http.exception';
 import multer from 'multer';
+import fs from 'fs';
 
 class userRouter implements RouteInterface {
   public router: Router = Router();
-  public upload : multer.Multer;
+  public upload: multer.Multer;
   constructor() {
-    this.upload = multer({ dest : 'uploads/'});
+    this.upload = multer({ dest: 'uploads/' });
     this.initializeRoutes()
   }
 
@@ -17,21 +18,25 @@ class userRouter implements RouteInterface {
     this.router.post('/login', this.upload.none(), this.login);
   }
   private register = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
-    console.log(req);
+    const { firstName, lastName, email, password } = req.body;
+    const avatar = req.file?.path as string;
     try {
-      const {firstName , lastName , email , password } = req.body;
-      const avatar = req.file?.path as string;
-      const userToken = await userController.signUp({firstName , lastName , email , password , avatar});
+      const userToken = await userController.signUp({ firstName, lastName, email, password, avatar });
       res.status(200).json({ token: userToken });
     } catch (error: any) {
-      next(new httpException(401, error.message as string));
+        try {
+          fs.unlinkSync(avatar);
+        } catch (error) {
+          console.log(error);
+        }
+        next(new httpException(401, error.message as string));
     }
   }
 
   private login = async (req: Request, res: Response, next: NextFunction): Promise<Response | void> => {
     try {
       const userToken = await userController.login(req.body);
-     
+
       res.status(200).json({ token: userToken });
     } catch (error: any) {
       next(new httpException(401, error.message as string));
