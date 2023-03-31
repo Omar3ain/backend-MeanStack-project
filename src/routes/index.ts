694452b897682d843877a0192d5066d83ceac3be
Muhammed Adel <muhammed.adel.elshall@gmail.com>
2023-03-31 13:27:26 +0200

    resolve confilcties

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,8 @@ import RouteInterface from '@/utils/interfaces/router.interface';
 import adminRouter from '@/routes/admin/index'
 import userRouter from '@/routes/user/user';
 import bookRouter from '@/routes/user/book';
-import authorRouter from './user/author';
-
 import categoryRouter from '@/routes/user/category';
+import authorRouter from './user/author';
 
 
 class mainRouter implements RouteInterface {
@@ -19,6 +18,8 @@ class mainRouter implements RouteInterface {
     this.router.use(`${this.path}admin`, new adminRouter().router);
     this.router.use(`${this.path}users`, new userRouter().router);
     this.router.use(`${this.path}books`, new bookRouter().router);
+    this.router.use(`${this.path}categories`, new categoryRouter().router);
+    this.router.use(`${this.path}authors`, new authorRouter().router);
   }
 
 }
