2403bd1a244175e3e2e06a2d6c761e5a6c884c6a
omar3ain <omar3ain@gmail.com>
2023-03-31 19:43:06 +0200

    updated update book schema

--- src/utils/validations/book/schema.ts
+++ src/utils/validations/book/schema.ts
@@ -19,11 +19,11 @@ const createBook =  joi.object().keys({
 });
 
 const updateBook =  joi.object().keys({
-  coverPhoto : joi.string(),
-  name : joi.string().min(5).max(20),
-  shelve : joi.string().valid('read', 'want_to_read', 'currently_reading' , 'none'),
-  description: joi.string(),
-  reviews : joi.array().items(reviews)
+  coverPhoto : createBook.extract('firstName'),
+  name : createBook.extract('name').optional(),
+  shelve : createBook.extract('shelve'),
+  description: createBook.extract('description').optional(),
+  reviews : createBook.extract('reviews')
 });
 
 
