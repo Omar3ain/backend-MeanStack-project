00d17a4d7d61dbcaa0c6a96d987602b174fde583
omar3ain <omar3ain@gmail.com>
2023-03-31 19:45:52 +0200

    solved the conflict in book schema file

--- src/utils/validations/book/schema.ts
+++ src/utils/validations/book/schema.ts
@@ -17,21 +17,12 @@ const createBook =  joi.object({
   reviews : joi.array().items(reviews)
 });
 
-<<<<<<< HEAD
 const updateBook =  joi.object().keys({
   coverPhoto : createBook.extract('firstName'),
   name : createBook.extract('name').optional(),
   shelve : createBook.extract('shelve'),
   description: createBook.extract('description').optional(),
   reviews : createBook.extract('reviews')
-=======
-const updateBook =  joi.object({
-  coverPhoto : joi.string(),
-  name : joi.string().min(5).max(20),
-  shelve : joi.string().valid('read', 'want_to_read', 'currently_reading' , 'none'),
-  description: joi.string(),
-  reviews : joi.array().items(reviews)
->>>>>>> 9efa544c3db8de7dedc60d577086b8b487df7015
 });
 
 
