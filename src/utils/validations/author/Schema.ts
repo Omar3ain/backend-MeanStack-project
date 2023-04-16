084830f66218412cac71426567a876c6687e358c
hayah1999 <hayahhazem1999@gmail.com>
2023-04-16 14:21:08 +0200

    joi modification2

--- src/utils/validations/author/Schema.ts
+++ src/utils/validations/author/Schema.ts
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const createAuthorSchema = Joi.object({
     firstName: Joi.string().trim().min(5).max(20).required(),
     lastName: Joi.string().trim().min(5).max(20).required(),
-    dob: Joi.date().trim().required(),
+    dob: Joi.date().required(),
     photo: Joi.string().trim(),
     description: Joi.string().trim(),
 });
